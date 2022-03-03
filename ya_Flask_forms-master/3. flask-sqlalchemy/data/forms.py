8fa2572f5b70e0bb3a35e377892b17f249c93830
Your Name <you@example.com>
2022-03-03 17:43:55 +0300

    Кусок с первыми тремя задачами

--- ya_Flask_forms-master/3. flask-sqlalchemy/data/forms.py
+++ ya_Flask_forms-master/3. flask-sqlalchemy/data/forms.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.fields.html5 import EmailField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 
 
@@ -9,5 +8,13 @@ class RegisterForm(FlaskForm):
     login = StringField('Login/email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm = PasswordField('Repeat password', validators=[DataRequired()])
-    surname = StringField('Surname')
-    submit = SubmitField('Submit')
+    name = StringField('Name', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    submit = SubmitField('Submit', validators=[DataRequired()])
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
